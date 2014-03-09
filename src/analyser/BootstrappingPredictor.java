f72c583db76f9272fd36170cc38da5451fab00af
smoiz <xxxsmoizxxx@gmail.com>
2014-03-09 22:02:57 +0100

    Bootstrapping now selects optimal sampling method automatically.

--- src/analyser/BootstrappingPredictor.java
+++ src/analyser/BootstrappingPredictor.java
@@ -1,6 +1,8 @@
 package analyser;
 
 import net.sf.javaml.classification.Classifier;
+import net.sf.javaml.classification.KDtreeKNN;
+import net.sf.javaml.classification.KNearestNeighbors;
 import net.sf.javaml.classification.meta.Bagging;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.sampling.Sampling;
@@ -15,6 +17,7 @@ import java.util.*;
 public class BootstrappingPredictor extends StockPredictor {
 
     private List<StockPredictor> predictors;
+    private Sampling samplingMethod;
 
     public BootstrappingPredictor(List<StockPredictor> predictors) {
         super();
@@ -37,7 +40,7 @@ public class BootstrappingPredictor extends StockPredictor {
             }
             sb.append(acronym).append(", ");
         }
-        return sb.substring(0, sb.length() - 2);
+        return String.format("%s from: %s", samplingMethod, sb.substring(0, sb.length() - 2));
     }
 
     /**
@@ -47,11 +50,27 @@ public class BootstrappingPredictor extends StockPredictor {
      */
     @Override
     protected Classifier classifier(Dataset dataset) {
+
         Classifier[] classifiers = new Classifier[predictors.size()];
         for(int i = 0; i < classifiers.length; i++){
             classifiers[i] = predictors.get(i).classifier(dataset);
         }
+
+        Classifier classifier = null;
+        accuracy = 0.0;
         Random rnd = new Random(4325435);
-        return new Bagging(classifiers, Sampling.StratifiedNormalBootstrapping, rnd.nextLong());
+        for(Sampling sampling : Sampling.values()){
+
+            classifier = new Bagging(classifiers, sampling, rnd.nextLong());
+            double newAccuracy = crossValidateAccuracy(classifier, dataset);
+            if (newAccuracy > accuracy) {
+                samplingMethod = sampling;
+                accuracy = newAccuracy;
+            }
+
+        }
+
+        return classifier;
+
     }
 }
